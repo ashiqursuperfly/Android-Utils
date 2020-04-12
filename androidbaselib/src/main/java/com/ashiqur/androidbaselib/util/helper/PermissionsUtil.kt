9992e9b91adb8c43738244bf7454d153efb75098
ashiqur.superfly <ashiqur.buet16@gmail.com>
2020-04-12 21:07:37 +0600

    handle multiple permissions

--- androidbaselib/src/main/java/com/ashiqur/androidbaselib/util/helper/PermissionsUtil.kt
+++ androidbaselib/src/main/java/com/ashiqur/androidbaselib/util/helper/PermissionsUtil.kt
@@ -4,11 +4,23 @@ import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.karumi.dexter.listener.single.PermissionListener
 
 /* Created by ashiq.buet16 **/
 
 object PermissionsUtil {
+
+    fun requestPermissions(
+        activity: Activity,
+        permission: ArrayList<String>,
+        listener: MultiplePermissionsListener
+    ) {
+
+        Dexter.withActivity(activity)
+            .withPermissions(permission)
+            .withListener(listener).check()
+    }
 
     fun requestPermission(
         context: Context,
@@ -19,7 +31,9 @@ object PermissionsUtil {
         if (!isPermissionAllowed(context,permission)) {
             Dexter.withActivity(activity)
                 .withPermission(permission)
-                .withListener(listener).check()
+                .withListener(listener)
+                .onSameThread()
+                .check()
         }
     }
 
@@ -32,14 +46,4 @@ object PermissionsUtil {
         // Timber.d("Permission Status:$hasPermission")
         return hasPermission == PackageManager.PERMISSION_GRANTED
     }
-
-    fun isPermissionDenied(context: Context, permission: String): Boolean {
-        val pm: PackageManager = context.packageManager
-        val hasPermission = pm.checkPermission(
-            permission,
-            context.packageName
-        )
-        return hasPermission == PackageManager.PERMISSION_DENIED
-    }
-
 }
